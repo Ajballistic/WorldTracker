6e9efd10b7908612b30770e8c0d8197829335dde
AJ <ajballistic24@gmail.com>
2025-10-13 14:50:13 +0100

    made toggleable stat buttons

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import json
+import copy
 
 app = Flask(__name__)
 
@@ -7,7 +8,10 @@ app = Flask(__name__)
 def index():
     with open('a6f2f5da-5773-4432-b7b4-8ec0b34a104a.json', 'r') as file:
         data = json.load(file)
-    return render_template("index.html", stats = data["stats"])
+        new_data = copy.deepcopy(data)
+        for stat_type in data["stats"]:
+            new_data["stats"][stat_type.replace("minecraft:","")] = new_data["stats"].pop(stat_type)
+    return render_template("index.html", stats = new_data["stats"])
 
 if __name__ == "__main__":
     app.run()
